1f8ad151ac01e735338517ececb4b8032afd2bf2
dipaligaikwad12 <gdipali070@gmail.com>
2025-02-05 19:00:49 +0530

    ui created

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const  WEBSITE_NAME = "Happy Littles Clothing Outlet";
+const WEBSITE_DESCRIPTION = 'Children, regardless of gender, shared styles and cuts before the twentieth century. From the sixteenth to the twentieth centuries, both men and women wore gowns, tunics, and robes. The gown became a thing for women, newborns, and toddlers only after men attire evolved into two-piece clothes, shirts, and breeches. Children styles evolved from gowns to adult garments as they grew older.';
+const HEADER_IMG = 
+"https://img.freepik.com/free-photo/little-customer-standing-store-choosing-new-dresses_7502-4255.jpg?t=st=1738677719~exp=1738681319~hmac=01947b2d2fb2b48eb2f9d216f5b354e8726eb0a4de762a5720f1a94044c413d5&w=740";
+
+const BUTTON1 = "By Now";
+const BUTTON2 = "Contact Us";
+
+const BUTTON_STYLES = {
+    "background-color": "green",
+    "border-radius": "10px",
+    "border": "none",
+    "width":"150px",
+    "font-size":"20px",
+    "cursor":"pointer"
+}
+
+export {WEBSITE_NAME, WEBSITE_DESCRIPTION, HEADER_IMG, BUTTON1, BUTTON2, BUTTON_STYLES };
