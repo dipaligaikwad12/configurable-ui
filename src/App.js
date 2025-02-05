6127bf5f904d9e3e8fa8865d4226a16913d9cab5
dipaligaikwad12 <gdipali070@gmail.com>
2025-02-05 19:09:37 +0530

    ui completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { WEBSITE_NAME,WEBSITE_DESCRIPTION, HEADER_IMG, BUTTON1, BUTTON2, BUTTON_STYLES,
-
 } from "./config";
+import "./App.css"
+
 
 function App () {
   return (
     <div>
-      <h1 className='text-4xl text-blue-900 justify-center'>{WEBSITE_NAME}</h1>
-      <p className='text-6m text-slate-700 mx-10 my-5'>{WEBSITE_DESCRIPTION}
+      <h1 className='text-4xl text-blue-900 text-center mt-6'>{WEBSITE_NAME}</h1>
+      <p className='text-lg text-slate-700 mx-10 my-5'>{WEBSITE_DESCRIPTION}
       </p>
       
-      <img src= {HEADER_IMG} className="h-100 rounded-3xl border-2 block mx-auto" alt="image"/>
+      <img src= {HEADER_IMG} className="h-80 rounded-3xl border-2 block mx-auto" alt="image"/>
       <div className="flex justify-center">
         <button className=" m-5 px-6  text-white rounded-md" style={BUTTON_STYLES}>
           {BUTTON1}
